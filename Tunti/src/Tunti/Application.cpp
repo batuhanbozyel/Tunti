de01ff842ca9f2495789c20e86bda9853af009b7
batuhanbozyel <batuhanbozyel@users.noreply.github.com>
2020-05-01 00:59:43 +0300

    Window Events added

--- Tunti/src/Tunti/Application.cpp
+++ Tunti/src/Tunti/Application.cpp
@@ -1,26 +1,47 @@
 #include "tpch.h"
 #include "Application.h"
 
-#include "Tunti/Events/ApplicationEvent.h"
 #include "Tunti/Log.h"
+
+#include <GLFW/glfw3.h>
 
 namespace Tunti
 {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 	}
 
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		T_CORE_TRACE("{0}", e);
+	}
+
 	void Application::Run()
 	{
 		while (m_Running) 
 		{
+			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
 
