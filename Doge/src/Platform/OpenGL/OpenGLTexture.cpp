96633abb7a3c45f19f173a2a7aa38b6b8e093b1c
batuhanbozyel <batuhanbozyel@users.noreply.github.com>
2020-10-05 23:50:12 +0300

    OpenGL Cubemap implementation

--- Doge/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Doge/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -64,4 +64,53 @@ namespace Doge
 	{
 		return glGetTextureHandleARB(m_ID);
 	}
+
+	// OpenGLCubemapTexture
+
+	OpenGLCubemapTexture::OpenGLCubemapTexture(const std::array<std::string, 6>& cubemapTexturePaths)
+	{
+		struct CubemapFace
+		{
+			unsigned char* buffer;
+			int width, height, channels;
+		} faces[6];
+
+		stbi_set_flip_vertically_on_load(1);
+
+		glCreateTextures(GL_TEXTURE_CUBE_MAP, 1, &m_ID);
+
+		for(uint32_t i = 0; i < 6; i++)
+			faces[i].buffer = stbi_load(cubemapTexturePaths[i].c_str(), &faces[i].width, &faces[i].height, &faces[i].channels, 4);
+
+		glTextureStorage2D(m_ID, 1, GL_RGBA8, faces[0].height, faces[0].height);
+
+		glTextureParameteri(m_ID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTextureParameteri(m_ID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTextureParameteri(m_ID, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTextureParameteri(m_ID, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTextureParameteri(m_ID, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		for (uint32_t i = 0; i < 6; i++)
+		{
+			CubemapFace face = faces[i];
+
+			if (face.buffer)
+				glTextureSubImage3D(m_ID, 0, 0, 0, i, face.height, face.height, 1, GL_RGBA, GL_UNSIGNED_BYTE, face.buffer);
+
+			else
+				Log::Error("Cubemap Texture failed to load face: {0}", cubemapTexturePaths[i]);
+
+			stbi_image_free(face.buffer);
+		}
+	}
+
+	OpenGLCubemapTexture::~OpenGLCubemapTexture()
+	{
+		glDeleteTextures(1, &m_ID);
+	}
+
+	void OpenGLCubemapTexture::Bind(uint32_t slot) const
+	{
+		glBindTextureUnit(slot, m_ID);
+	}
 }
--- Doge/src/Platform/OpenGL/OpenGLTexture.h
+++ Doge/src/Platform/OpenGL/OpenGLTexture.h
@@ -10,7 +10,18 @@ namespace Doge
 		explicit OpenGLTexture(const std::string& texturePath);
 		~OpenGLTexture();
 	private:
-		virtual uint64_t GetTextureHandle();
+		virtual uint64_t GetTextureHandle() override;
+	private:
+		uint32_t m_ID;
+	};
+
+	class OpenGLCubemapTexture : public CubemapTexture
+	{
+	public:
+		explicit OpenGLCubemapTexture(const std::array<std::string, 6>& cubemapTexturePaths);
+		~OpenGLCubemapTexture();
+
+		virtual void Bind(uint32_t slot) const override;
 	private:
 		uint32_t m_ID;
 	};
