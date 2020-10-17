e05bdcbe16e5bba60375d57b6c52026eb1aa75fa
batuhanbozyel <batuhanbozyel@users.noreply.github.com>
2020-10-17 12:40:45 +0300

    Time class code design change

--- Doge/src/Doge/Core/Time.h
+++ Doge/src/Doge/Core/Time.h
@@ -6,24 +6,38 @@ namespace Doge
 	class Time
 	{
 	public:
-		static void Init()
-		{
-			if(!s_Instance)
-				s_Instance = CreateScope<Time>();
-		}
-
-		static void Tick()
+		inline void OnTick()
 		{
 			float time = static_cast<float>(glfwGetTime());
-			s_Instance->m_DeltaTime = (time - s_Instance->m_LastFrameTime) * 1000.0f;
-			s_Instance->m_LastFrameTime = time;
+			m_DeltaTime = (time - m_LastFrameTime) * 1000.0f;
+			m_LastFrameTime = time;
 		}
 
-		static float DeltaTime() { return s_Instance->m_DeltaTime; }
+		static inline float DeltaTime()
+		{
+			return s_Instance->m_DeltaTime;
+		}
+
+		static inline Time& GetInstance()
+		{
+			if (!s_Instance)
+				s_Instance = new Time;
+
+			return *s_Instance;
+		}
+
+		Time(const Time& other) = delete;
+		Time& operator=(const Time& other) = delete;
+	private:
+		Time() = default;
+		~Time()
+		{
+			s_Instance = nullptr;
+		}
 	private:
 		float m_LastFrameTime = 0.0f;
 		float m_DeltaTime = 0.0f;
 
-		static Scope<Time> s_Instance;
+		static Time* s_Instance;
 	};
 }
