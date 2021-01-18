bc21b7745b4f8d476975e83a8dd9e3065b1fdda6
batuhanbozyel <batuhanbozyel@users.noreply.github.com>
2021-01-18 18:11:59 +0300

    Removed redundant inline keyword

--- Doge/src/Doge/Core/Log.h
+++ Doge/src/Doge/Core/Log.h
@@ -10,19 +10,19 @@ namespace Doge
 		static void Init();
 
 		template<typename ... Args>
-		inline static void Trace(Args&& ... args) { s_Logger->trace(std::forward<Args>(args)...); }
+		static void Trace(Args&& ... args) { s_Logger->trace(std::forward<Args>(args)...); }
 
 		template<typename ... Args>
-		inline static void Info(Args&& ... args) { s_Logger->info(std::forward<Args>(args)...); }
+		static void Info(Args&& ... args) { s_Logger->info(std::forward<Args>(args)...); }
 
 		template<typename ... Args>
-		inline static void Warn(Args&& ... args) { s_Logger->warn(std::forward<Args>(args)...); }
+		static void Warn(Args&& ... args) { s_Logger->warn(std::forward<Args>(args)...); }
 
 		template<typename ... Args>
-		inline static void Error(Args&& ... args) { s_Logger->error(std::forward<Args>(args)...); }
+		static void Error(Args&& ... args) { s_Logger->error(std::forward<Args>(args)...); }
 
 		template<typename ... Args>
-		inline static void Critical(Args&& ... args) { s_Logger->critical(std::forward<Args>(args)...); }
+		static void Critical(Args&& ... args) { s_Logger->critical(std::forward<Args>(args)...); }
 	private:
 		static Ref<spdlog::logger> s_Logger;
 	};
